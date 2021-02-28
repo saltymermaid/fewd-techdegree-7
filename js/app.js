68da44a14ed193f3de9912ba22c37a6e74c82f9e
Melissa Blades <melissa@saltymermaid.dev>
2021-02-28 10:48:59 -0500

    Add styles and charts

---
+++ js/app.js
@@ -0,0 +1,67 @@
+let traffic = document.getElementById('traffic').getContext('2d');
+let trafficChart = new Chart(traffic, {
+  type: 'line',
+  data: {
+    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+    datasets: [{
+      label: 'Traffic',
+      data: [500, 1000, 600, 1250, 1750, 1100, 1500, 1000, 1500, 2000, 1500, 2000],
+      // backgroundColor: '#CA2085',
+      lineTension: 0,
+      fill: false,
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+let dailyTraffic = document.getElementById('daily-traffic').getContext('2d');
+let dailyTrafficChart = new Chart(dailyTraffic, {
+  type: 'bar',
+  data: {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [{
+      label: 'Daily Traffic',
+      data: [50, 75, 150, 100, 200, 175, 75],
+      // backgroundColor: '#CA2085',
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+let mobileUsers = document.getElementById('mobile-users').getContext('2d');
+let mobileUsersChart = new Chart(mobileUsers, {
+  type: 'doughnut',
+  data: {
+    labels: ['Desktop', 'Phones', 'Tablets'],
+    datasets: [{
+      label: 'Mobile Users',
+      data: [70, 15, 15],
+      // backgroundColor: ['#CA2085', '#D29506', '#55414A'],
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
